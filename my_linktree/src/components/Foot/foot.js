4c4fe663a34da165a7c34fee82ddd2d4a9e18bb1
amarapeace <onwunzoamara@yahoo.com>
2022-10-28 21:11:16 +0100

    margin set

--- my_linktree/src/components/Foot/foot.js
+++ my_linktree/src/components/Foot/foot.js
@@ -5,9 +5,6 @@ import zuriintern from '../assets/zuri-intern.png'
 export default function Foot() {
     return (
       <>
-        {/* <div>Zuri Internship</div>
-        <div>HNG Internship 9 Frontend Task</div>
-        <div>INGRESSIVE FOR GOOD</div> */}
         <footer className='foot'>
           <img src={zuriintern} alt="" />
           <img src={midfooter} alt="" />
